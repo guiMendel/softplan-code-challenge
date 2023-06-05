98f33d66afd76d36041e31d5681de934a5520042
guimendel <guimendeln@gmail.com>
2023-06-05 10:01:03 -0300

    🐛Fix test file invalid prop passing

--- src/components/__tests__/InputField.spec.ts
+++ src/components/__tests__/InputField.spec.ts
@@ -2,23 +2,26 @@ import { describe, it, expect } from 'vitest'
 import { mount } from '@vue/test-utils'
 import InputField from '../InputField.vue'
 import { ref } from 'vue'
+import type { Field } from '@/modules/useUserField'
 
 describe('InputField', () => {
   it('should start showing placeholder', () => {
-    const field = ref({
+    const field = ref<Field>({
+      name: 'name',
       value: '',
       valid: false
     })
 
     const wrapper = mount(InputField, {
-      props: { modelValue: field.value, name: 'test' }
+      props: { modelValue: field.value }
     })
 
     expect(wrapper.text()).toContain('test')
   })
 
   it('should update model value', async () => {
-    const field = ref({
+    const field = ref<Field>({
+      name: 'name',
       value: '',
       valid: false
     })
@@ -29,8 +32,7 @@ describe('InputField', () => {
       props: {
         modelValue: field.value,
         'onUpdate:modelValue': (newValue: typeof field.value) => (field.value = newValue),
-        placeholder,
-        name: 'test'
+        placeholder
       }
     })
 
@@ -42,7 +44,8 @@ describe('InputField', () => {
   })
 
   it('should set valid to false when appropriate', async () => {
-    const field = ref({
+    const field = ref<Field>({
+      name: 'name',
       value: '',
       valid: false,
       validate: (newValue: string): string | true => 'example error'
@@ -51,8 +54,7 @@ describe('InputField', () => {
     const wrapper = mount(InputField, {
       props: {
         modelValue: field.value,
-        'onUpdate:modelValue': (newValue: typeof field.value) => (field.value = newValue),
-        name: 'test'
+        'onUpdate:modelValue': (newValue: typeof field.value) => (field.value = newValue)
       }
     })
 
@@ -62,7 +64,8 @@ describe('InputField', () => {
   })
 
   it('should set valid to true when field is valid', async () => {
-    const field = ref({
+    const field = ref<Field>({
+      name: 'name',
       value: '',
       valid: false,
       validate: (newValue: string): string | true => true
@@ -71,8 +74,7 @@ describe('InputField', () => {
     const wrapper = mount(InputField, {
       props: {
         modelValue: field.value,
-        'onUpdate:modelValue': (newValue: typeof field.value) => (field.value = newValue),
-        name: 'test'
+        'onUpdate:modelValue': (newValue: typeof field.value) => (field.value = newValue)
       }
     })
 
