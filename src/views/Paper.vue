aa27941d02fb0a064bbfc1e43180599b72999bcb
guimendel <guimendeln@gmail.com>
2023-06-05 23:30:27 -0300

    🔨Paper pass tests

--- src/views/Paper.vue
+++ src/views/Paper.vue
@@ -1,3 +1,15 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const textContent = ref('')
+</script>
+
 <template>
-  Write on me bro
+  <div id="paper">
+    <!-- Input field -->
+    <textarea id="input-area" v-model="textContent"></textarea>
+
+    <!-- Output view -->
+    <div id="output-area" v-html="textContent"></div>
+  </div>
 </template>
